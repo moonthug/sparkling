8e6b900e4c45130fc9dbc59e71fe431664da72cb
alex-givenergy <alex.coulcher@givenergy.co.uk>
2025-07-20 15:17:55 +0100

    fix: assets

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './sparkling',
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
